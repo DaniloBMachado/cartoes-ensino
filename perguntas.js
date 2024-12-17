da836477d81d6e90ef33b44ca5eeb280ce0cbee6
DaniloBMachado <boconcelo@prof.educacao.sp.gov.br>
2024-12-17 07:38:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'História',
+    'Quem foi Dom Pedro I do Brasil?',
+   'Dom Pedro I foi o primeiro imperador do Brasil, responsável por declarar a independência do país de Portugal em 7 de setembro de 1822.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior deserto do mundo?',
+    'O maior deserto do mundo é o Deserto da Antártica, devido às suas condições de baixa umidade e vastas áreas de gelo. O maior deserto quente é o Saara.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que são planetas do sistema solar classificados como "rochosos"?',
+    'Os planetas rochosos são aqueles compostos principalmente por rochas e metais. No Sistema Solar, esses planetas são Mercúrio, Vênus, Terra e Marte.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro "Dom Casmurro"?',
+    'O livro "Dom Casmurro" foi escrito por Machado de Assis, um dos maiores escritores brasileiros, conhecido por sua crítica social e narrativa introspectiva.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é HTML?',
+    'HTML (ou HyperText Markup Language) é a linguagem padrão usada para criar e estruturar conteúdo na web.'
+)
+
+criaCartao(
+    'Artes',
+    'O que é o Renascimento?',
+    'O Renascimento foi um movimento cultural e artístico ocorrido entre os séculos XIV e XVII, que marcou a transição da Idade Média para a Modernidade, caracterizado pela valorização da cultura clássica e do humanismo.'
+)
+
+criaCartao(
+    'Física',
+    'O que é gravidade?',
+    'Gravidade é a força de atração que corpos com massa exercem entre si. Na Terra, ela é responsável por manter os objetos no solo e os planetas em órbita no sistema solar.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a função do DNA no corpo humano?',
+    'O DNA (também conhecido como ácido desoxirribonucleico) contém as informações genéticas que determinam as características biológicas de um organismo e orienta o funcionamento das células.'
+)
+
+criaCartao(
+    'Química',
+    'O que é a tabela periódica?',
+    'A tabela periódica é uma organização dos elementos químicos com base em suas propriedades químicas e físicas, dispostos em ordem crescente de número atômico.'
+)
